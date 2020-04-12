359714b25b48e4e736b94a0a985049f1b65d9fdb
hienpham <hienpham@b13technology.com>
2020-04-12 17:40:31 +0700

    Init code

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import {Module} from "@nestjs/common";
+import {AppController} from "./app.controller";
+
+@Module({
+  imports: [
+  ],
+  controllers: [
+      AppController
+  ]
+})
+export class AppModule {
+
+}
